e10b88a0b9144edf43435486098d4f493ca2137c
Gabriel Loyola Daiqui <git@loyola.dev>
2024-02-16 21:12:38 +0100

    Adds optional flag to pass working directory

--- main.go
+++ main.go
@@ -1,25 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
 func main() {
-	wd, err := os.Getwd()
+	path, err := getPath()
 	if err != nil {
-		fmt.Printf("Failed to get working directory: %s\n", wd)
+		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	moduleName, err := getModuleName(wd)
+	moduleName, err := getModuleName(path)
 	if err != nil {
 		fmt.Printf("Error: %s\n", err)
 		os.Exit(1)
 	}
 
 	m := make(map[string]os.FileMode)
-	m, err = getFileList(wd, m)
+	m, err = getFileList(path, m)
 	if err != nil {
 		fmt.Printf("Error: %s\n", err)
 		os.Exit(1)
@@ -42,4 +43,37 @@ func main() {
 	}
 
 	fmt.Printf("All %d Go files were succesfully formatted.", len(m))
+}
+
+func getPath() (string, error) {
+	var path string
+	flag.StringVar(&path, "path", "", "path to format")
+	flag.Parse()
+
+	if len(path) == 0 {
+		// No path given => Use working directory
+		wd, err := os.Getwd()
+		if err != nil {
+			return "", fmt.Errorf("Failed to get working directory: %s\n", wd)
+		}
+
+		return wd, nil
+	}
+
+	// Path given => Check if it exists
+	f, err := os.Stat(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return "", fmt.Errorf("the path %s does not exist", path)
+		}
+
+		return "", err
+	}
+
+	// Verify we were given a directory
+	if !f.IsDir() {
+		return "", fmt.Errorf("the provided path does not lead to a directory")
+	}
+
+	return path, nil
 }
